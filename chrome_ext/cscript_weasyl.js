26bc720df86ec3ea90625735dbdb9d9f1b0347f7
Kakurady <kakurady@gmail.com>
2017-10-15 16:32:37 -0400

    Fill in the description box with the file name(s).

--- chrome_ext/cscript_weasyl.js
+++ chrome_ext/cscript_weasyl.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+"use strict";
+var box = document.getElementById("submitfile");
+
+async function fileChanged(){
+    var files = [...box.files];
+    
+    // substitute with actual metadata gathering
+    var filenames = files.map( x=> x.name );
+    var description = filenames.join("\n");
+    
+    var descbox = document.getElementById("submissiondesc")
+    descbox.value = description;
+    descbox.dispatchEvent(new Event("input")); // keeping markdown converter happy
+    descbox.dispatchEvent(new Event("change")); // not useful here, but the tag list builder needs it.
+}
+
+
+box.addEventListener("change", fileChanged);
