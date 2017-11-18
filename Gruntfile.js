87bb50aaf2fd801d031143058107c204c26bf5c3
Kakurady <kakurady@gmail.com>
2017-11-18 16:43:40 -0500

    Got Weasyl form fill working in Chrome!

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,42 @@ module.exports = function(grunt) {
                     }
                 ]
             }
+        },
+        browserify:{
+            'node-ts-iptc': {
+                files: [
+                    {
+                        src: "node_modules/ts-node-iptc/index.js", 
+                        dest: "chrome_ext/ext/ts-node-iptc/IptcReader.js"
+                    }
+                ],
+                options: {
+                    browserifyOptions:{
+                        debug: true,
+                        standalone: "IptcParser"
+                    }
+                }
+            },
+            // FIXME: don't actually need all function of Node's buffer, can do with much less
+            'buffer': {
+                files: [
+                    {
+                        src: "node_modules/buffer/index.js", 
+                        dest: "chrome_ext/ext/buffer/buffer.js"
+                    }
+                ],
+                options: {
+                    browserifyOptions:{
+                        standalone: "buffer"
+                    }
+                }
+            }
         }
+        
     });
     
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-browserify');
     
-    grunt.registerTask('default', ['copy']);
+    grunt.registerTask('default', ['copy', 'browserify']);
 }
--- chrome_ext/cscript_weasyl.js
+++ chrome_ext/cscript_weasyl.js
@@ -1,18 +1,66 @@
 "use strict";
 var box = document.getElementById("submitfile");
 
+box.addEventListener("change", fileChanged);
+
 async function fileChanged(){
+    // read files
     var files = [...box.files];
     
-    // substitute with actual metadata gathering
-    var filenames = files.map( x=> x.name );
-    var description = filenames.join("\n");
+    let meta = await readMetadataFromFile(files[0]);
+    console.log(meta);
+    
+    // interpret
+    var title = meta.headline;
+    var description = meta.caption;
+    var tags = [...buildWeasylTags(meta)];
+    
+    // fill
+    var titlebox = document.getElementById("submissiontitle");
+    titlebox.value = title.slice(0, 200);
     
     var descbox = document.getElementById("submissiondesc")
     descbox.value = description;
     descbox.dispatchEvent(new Event("input")); // keeping markdown converter happy
     descbox.dispatchEvent(new Event("change")); // not useful here, but the tag list builder needs it.
+    
+    var tagbuilder =  document.querySelector("[name=tags]+.listbuilder input");
+    tagbuilder.value=tags.join(" ");
+    tagbuilder.dispatchEvent(new KeyboardEvent("keyup"));
 }
 
+function readFileAsArrayBufferAsync(file){
+    var reader = new FileReader();
+    return new Promise((resolve,reject) => {
+        try {
+            reader.onload = (evt) => resolve(reader.result);
+            reader.onerror = (evt) => reject(reader.error);
+            reader.onabort = (evt) => reject(evt);
+            reader.readAsArrayBuffer(file);
+        } catch (ex){
+            reject(ex);
+        }
+    });
 
-box.addEventListener("change", fileChanged);
+}
+
+async function readMetadataFromFile(file){
+    let arrayBuffer = await readFileAsArrayBufferAsync(file);
+
+    let nodeBuffer = buffer.Buffer.from(arrayBuffer);
+    let meta = IptcParser.IptcParser.parse(nodeBuffer);
+    
+    return meta;
+}
+
+function* buildWeasylTags(meta){
+    const underlined=/[ -]/gi;
+    for(const keyword of meta.keywords){
+        // TODO process "Pokémon"
+        yield keyword.replace(underlined, "_");
+    }
+    if (meta.city) yield meta.city;
+    if (meta.province_or_state) yield meta.province_or_state;
+    if (meta.country_or_primary_location_name) yield meta.country_or_primary_location_name;
+    
+}
