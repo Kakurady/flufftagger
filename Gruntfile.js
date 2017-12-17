df24ed6ada054729a94ba001e97cafecd8492222
Kakurady <kakurady@gmail.com>
2017-12-17 17:02:04 -0500

    Atom likes to remove blank lines at end of line

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,11 @@ module.exports = function(grunt) {
             'webextension-polyfill': {
                 files: [
                     {
-                        src: "node_modules/webextension-polyfill/dist/browser-polyfill.js", 
+                        src: "node_modules/webextension-polyfill/dist/browser-polyfill.js",
                         dest: "chrome_ext/ext/webextension-polyfill/browser-polyfill.js"
                     },
                     {
-                        src: "node_modules/webextension-polyfill/dist/browser-polyfill.js.map", 
+                        src: "node_modules/webextension-polyfill/dist/browser-polyfill.js.map",
                         dest: "chrome_ext/ext/webextension-polyfill/browser-polyfill.js.map"
                     }
                 ]
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
             'node-ts-iptc': {
                 files: [
                     {
-                        src: "node_modules/ts-node-iptc/index.js", 
+                        src: "node_modules/ts-node-iptc/index.js",
                         dest: "chrome_ext/ext/ts-node-iptc/IptcReader.js"
                     }
                 ],
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
             'buffer': {
                 files: [
                     {
-                        src: "node_modules/buffer/index.js", 
+                        src: "node_modules/buffer/index.js",
                         dest: "chrome_ext/ext/buffer/buffer.js"
                     }
                 ],
@@ -44,11 +44,11 @@ module.exports = function(grunt) {
                 }
             }
         }
-        
+
     });
-    
+
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-browserify');
-    
+
     grunt.registerTask('default', ['copy', 'browserify']);
 }
